4eea47de4deb26748c2e075cafad7e1c39a07fa5
吕柯 <1017833127@qq.com>
2015-04-14 14:39:11 +0800

    添加商定报价和修改训单和修改报价

--- app/src/main/java/nju/zhizaolian/fragments/ChangeQuoteFragment.java
+++ app/src/main/java/nju/zhizaolian/fragments/ChangeQuoteFragment.java
@@ -2,17 +2,20 @@ package nju.zhizaolian.fragments;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import nju.zhizaolian.R;
 
-public class SignContractFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChangeQuoteFragment extends Fragment {
 
 
-    public SignContractFragment() {
+    public ChangeQuoteFragment() {
         // Required empty public constructor
     }
 
@@ -20,9 +23,8 @@ public class SignContractFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        return inflater.inflate(R.layout.fragment_sign_contract,container);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_quote, container, false);
     }
 
 
